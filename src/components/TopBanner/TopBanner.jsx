ea7c598bea491b81e80ff42f2841006cc3d2d3c1
BiDi93 <abdmin9994061@gmail.com>
2024-08-26 15:53:30 +0800

    update top banner part1

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import "../../assets/css/custom.css";
+
+function TopBanner() {
+  return (
+    <Fragment>
+      <Container className="topFixedBanner"></Container>
+    </Fragment>
+  );
+}
+
+export default TopBanner;
