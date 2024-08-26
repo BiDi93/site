eccfaff09b247043eae66b96e470aa1e3b6c2396
BiDi93 <abdmin9994061@gmail.com>
2024-08-26 16:26:17 +0800

    done for top banner

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,11 +1,26 @@
 import React, { Fragment } from "react";
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "../../assets/css/custom.css";
+import "../../assets/css/bootsrap.min.css";
 
 function TopBanner() {
   return (
     <Fragment>
-      <Container className="topFixedBanner"></Container>
+      <Container fluid={true} className="topFixedBanner p-0">
+        <div className="topBannerOverlay">
+          <Container className="topContent">
+            <Row>
+              <Col className="text-center">
+                <h1 className="topTitle">
+                  ERP SOLUTION FOR SMALL MEDIUM ENTERPRISE
+                </h1>
+                <h4 className="topSubtitle">Learn profesionally</h4>
+                <Button variant="primary">More Info</Button>{" "}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Container>
     </Fragment>
   );
 }
